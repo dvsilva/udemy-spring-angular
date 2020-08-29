bab6a50ba3b0ebaa199b50fb74a3587ec8f8718b
dvsilva <dvsilva@gmail.com>
2020-08-29 09:06:57 -0300

    seção 7

--- frontend/clientes-app/src/app/app.module.ts
+++ frontend/clientes-app/src/app/app.module.ts
@@ -3,20 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello/hello.component';
-import { CursoAngularComponent } from './curso-angular/curso-angular.component';
-import { TesteComponent } from './teste/teste.component';
-
-import { ClientesModule } from './clientes/clientes.module';
-
+import { TemplateModule } from './template/template.module';
+import { HomeComponent } from './home/home.component';
 @NgModule({
-  declarations: [
-    AppComponent,
-    HelloComponent,
-    CursoAngularComponent,
-    TesteComponent,
-  ],
-  imports: [BrowserModule, AppRoutingModule, ClientesModule],
+  declarations: [AppComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, TemplateModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ frontend/clientes-app/src/app/template/template.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+@NgModule({
+  declarations: [NavbarComponent, SidebarComponent],
+  imports: [CommonModule],
+  exports: [NavbarComponent, SidebarComponent],
+})
+export class TemplateModule {}
